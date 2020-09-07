58cdbfa04dce5a9dd663d857dae64d399f98f428
alexeisamuta <alexeisamuta@gmail.com>
2020-09-07 14:24:26 +0300

    Updates

---
+++ precache-manifest.e8a0aa166298f6ac7d8089cfa0f343b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685befbbf21f31272da6559041c20322",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b09802e6bae9a300c8c7",
+    "url": "/portfolio/static/css/main.124d4a66.chunk.css"
+  },
+  {
+    "revision": "75a3168844590df7cc67",
+    "url": "/portfolio/static/js/2.eb5aa08e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.eb5aa08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09802e6bae9a300c8c7",
+    "url": "/portfolio/static/js/main.ae1b629b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
