f710f45cfa9f773459c2dec13b0fc191057b6b2f
alexeisamuta <alexeisamuta@gmail.com>
2020-10-09 16:48:06 +0300

    completed second task with corrections

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -4,9 +4,10 @@ import styleContainer from '../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
 import reactIcon from './../assets/image/react2.png'
-import TSIcon from './../assets/image/typescript.png'
-import CssIcon from './../assets/image/css.png'
+import TSIcon from './../assets/image/typescriptPNG.png'
+import CssIcon from './../assets/image/css1.png'
 import ReduxIcon from './../assets/image/redux.png'
+import HTMLIcon from './../assets/image/html.png'
 
 export function Skills() {
 
@@ -22,6 +23,9 @@ export function Skills() {
     const reduxImage = {
         backgroundImage: `url(${ReduxIcon})`,
     };
+    const HTMLImage = {
+        backgroundImage: `url(${HTMLIcon})`,
+    };
 
 
     return (
@@ -29,7 +33,8 @@ export function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={"My"} lastWord={"Skills"}/>
                 <div className={style.skills}>
-                    <Skill title={"TS"} description={"This section is dedicated to the JavaScript language itself"}
+                    <Skill title={"TypeScript"}
+                           description={"This section is dedicated to the JavaScript language itself"}
                            style={typescriptImage}/>
                     <Skill title={"CSS"}
                            description={"and not the parts that are specific to Web pages or other host environments."}
@@ -40,6 +45,9 @@ export function Skills() {
                     <Skill title={"Redux"}
                            description={"For information about API specifics to Web pages, please see Web APIs and DOM."}
                            style={reduxImage}/>
+                    <Skill title={"HTML"}
+                           description={"For information about API specifics to Web pages, please see Web APIs and DOM."}
+                           style={HTMLImage}/>
                 </div>
             </div>
         </div>
